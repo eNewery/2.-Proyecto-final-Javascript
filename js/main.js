301ba4715f564b3b62aa30b59ad29f75467cf134
Ulises <robdiaz247@gmail.com>
2022-07-07 21:17:00 -0300

    segundaproy

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
-let title = document.getElementById("titulo")
-let subtitle = document.getElementById("subtitulo")
+const Personas = []
 
-title.innerText = "He sido modificado desde JavaScript"
+function generarUsuario(){
+function Persona(name, age, email){
+    this.nombre = name;
+    this.edad = age;
+    this.correo = email;
+}
 
-let componentes = document.getElementsByClassName("componente")
 
-subtitle.innerHTML = "<h2>Componentes:</h2>"
+let preguntarNombre = document.getElementById("nombre").value
+let preguntarEdad = document.getElementById("edad").value
+let preguntarCorreo = document.getElementById("email").value
 
-componentes[0].innerText = "Memoria RAM"
-componentes[1].innerText = "Procesador"
-componentes[2].innerText = "Placa madre"
-componentes[3].innerText = "Placa de video"
+let persona = new Persona(preguntarNombre, preguntarEdad, preguntarCorreo)
+Personas.push(persona)
+localStorage.setItem("Personas", JSON.stringify(Personas))
+console.log(Personas)
+agregarUsuario(persona.nombre, persona.edad, persona.correo)
+}
+
+// DOM Code
+function agregarUsuario(user, age, email){
+let display = document.getElementById("users")
+display.innerHTML = "<p> User: " + user + "<br> Age: " + age + "<br> E-mail: " + email + "</p>"
+}
+
+
+
+let button = document.querySelector("#boton")
+button.addEventListener('keyup', generarUsuario)
+
