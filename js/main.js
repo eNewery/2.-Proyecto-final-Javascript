dbe5ebdada46ee55c398571e9c17ec19269099f0
Ulises <robdiaz247@gmail.com>
2022-07-14 20:21:40 -0300

    toastify

--- js/main.js
+++ js/main.js
@@ -1,32 +1,125 @@
+
+//Array en donde se almacenan los objetos Persona
 const Personas = []
 
-function generarUsuario(){
-function Persona(name, age, email){
-    this.nombre = name;
-    this.edad = age;
-    this.correo = email;
-}
+
+//Funcion constructora
+function generarUsuario() {
+    function Persona(name, age, email) {
+        this.nombre = name;
+        this.edad = age;
+        this.correo = email;
+    }
 
 
-let preguntarNombre = document.getElementById("nombre").value
-let preguntarEdad = document.getElementById("edad").value
-let preguntarCorreo = document.getElementById("email").value
+    //Aquí declaro las variables que dentro tienen los input
+    let preguntarNombre = document.getElementById("nombre").value
+    let preguntarEdad = document.getElementById("edad").value
+    let preguntarCorreo = document.getElementById("email").value
 
-let persona = new Persona(preguntarNombre, preguntarEdad, preguntarCorreo)
-Personas.push(persona)
-localStorage.setItem("Personas", JSON.stringify(Personas))
-console.log(Personas)
-agregarUsuario(persona.nombre, persona.edad, persona.correo)
+    let persona = new Persona(preguntarNombre, preguntarEdad, preguntarCorreo)
+
+    //Pusheo el objeto al array
+    Personas.push(persona)
+    displayError = document.querySelector("#users")
+    let name = document.getElementById("nombre")
+    let age = document.getElementById("edad")
+    let email = document.getElementById("email")
+    //Condicional de los input
+
+    if (persona.nombre === "") {
+        console.error("Los datos son incorrectos, no se han registrado cambios en el localStorage.")
+        name.classList.add("activaInput")
+        Toastify({
+            text: "Hubo un error en el nombre, inténtalo de nuevo!",
+            duration: 1000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // 
+            position: "left", // 
+            stopOnFocus: true, 
+            style: {
+              background: "linear-gradient(to right, #ff0000, #ff00aa)",
+            },
+            onClick: function(){} // 
+          }).showToast();  
+    }
+    else if (persona.edad === "") {
+        console.error("Los datos son incorrectos, no se han registrado cambios en el localStorage.")
+        age.classList.add("activaInput")
+        Toastify({
+            text: "Hubo un error en la edad, inténtalo de nuevo!",
+            duration: 1000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // 
+            position: "left", // 
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #ff0000, #ff00aa)",
+            },
+            onClick: function () { } // 
+        }).showToast();
+    }
+    else if (persona.correo === "") {
+        Toastify({
+            text: "Hubo un error en el e-mail, inténtalo de nuevo!",
+            duration: 1000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // 
+            position: "left", // 
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #ff0000, #ff00aa)",
+            },
+            onClick: function () { } // 
+        }).showToast();
+        console.error("Los datos son incorrectos, no se han registrado cambios en el localStorage.")
+        email.classList.add("activaInput")
+    }
+
+    else {
+        //Finalmente, si los datos son correctos son guardados en el localStorage
+        name.classList.remove("activaInput")
+        age.classList.remove("activaInput")
+        email.classList.remove("activaInput")
+        localStorage.setItem("Personas", JSON.stringify(Personas))
+        console.info("Los datos se han guardado satisfactoriamente.")
+        console.log(Personas)
+        //Es ejecutada la función agregarUsuario *
+
+        Toastify({
+            text: "Usuario registrado correctamente!",
+            duration: 1000,
+            newWindow: true,
+            close: true,
+            gravity: "top", // 
+            position: "left", // 
+            stopOnFocus: true,
+            style: {
+                background: "linear-gradient(to right, #AAaaaa, #aaaabb)",
+            },
+            onClick: function () { } // 
+        }).showToast();
+        agregarUsuario(persona.nombre, persona.edad, persona.correo)
+    }
 }
 
 // DOM Code
-function agregarUsuario(user, age, email){
-let display = document.getElementById("users")
-display.innerHTML = "<p> User: " + user + "<br> Age: " + age + "<br> E-mail: " + email + "</p>"
+function agregarUsuario(user, age, email) {
+    //Declaro la variable en donde será guardado el recuadro donde se visualizará el último usuario registrado
+    let display = document.getElementById("users")
+    saveLocal = JSON.parse(localStorage.getItem("Personas"))
+    for (items in saveLocal) {
+        console.log(items)
+    }
+    display.innerHTML = "<p> Usuario: " + user + "<br> Edad: " + age + "<br> Correo: " + email + "</p>"
 }
 
 
 
 let button = document.querySelector("#boton")
 button.addEventListener('keyup', generarUsuario)
+button.addEventListener('click', generarUsuario)
 
